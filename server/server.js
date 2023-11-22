1f3f0599f9d2e6aaa9714e197e58afd56e8ca888
furqan-abid <furqanabid10@gmail.com>
2023-11-22 14:51:03 +0500

    deploy

---
+++ server/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const  userRouter  = require("./routes/user-routes");
+const blogRouter = require("./routes/blog-routes");
+const cors = require('cors');
+const connectDatabase = require('./config/db');
+require('dotenv').config({path: 'config/config.env'});
+const path = require('path')
+const app = express();
+
+const _dirname = path.dirname('')
+
+const buildPath = path.join(_dirname,"../client/build")
+
+app.use(express.static(buildPath))
+
+app.get("/*",function(req,res){
+    res.sendFile(__dirname,"../client/build/index.html"),
+    function(err){
+        if(err){
+            res.status(500).send(err)
+        }
+    }
+})
+
+
+
+app.use(cors());
+
+app.set("view engine","ejs");
+app.use(express.json());
+
+app.use("/api/users",userRouter);
+app.use("/api/blogs",blogRouter);
+
+app.use("/api",(req,res,next) =>{
+    res.send("hello")
+})
+
+//define port
+
+
+//handling uncaught exceptions
+process.on('uncaughtException', function(err) {
+    console.log("Error: " + err.message);
+    console.log('shutting down the server due to uncaught exception')
+    process.exit(1);
+})
+
+
+//connecting database
+connectDatabase()
+
+const server= app.listen(process.env.Port,()=>{
+    console.log(`server is runing on ${process.env.Port}`);
+})
+
+//Unhandled promise rejection
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error: ${err}`);
+    console.log(`shutting down the server due to unhandled promise rejection`);
+    server.close(()=>{
+        process.exit(1);
+    })
+})
